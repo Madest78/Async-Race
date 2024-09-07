29031386ca882d9abaf5cfbd79d429b6dd310d5d
YuryMatusevich <vfnectdbx78@gmail.com>
2024-09-07 17:45:04 +0400

    feat: add routing garage>winners, winners>garage
    fix: fixig dependencies from eslint

--- async-race/src/app/app.component.ts
+++ async-race/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
+import { GarageComponent } from './core/components/pages/app.garage/garage.component';
+import { WinnersComponent } from './core/components/pages/app.winners/winners.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, GarageComponent, WinnersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- async-race/src/app/app.routes.ts
+++ async-race/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GarageComponent } from './core/components/pages/app.garage/garage.component';
+import { WinnersComponent } from './core/components/pages/app.winners/winners.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'app-garage', component: GarageComponent },
+  { path: 'app-winners', component: WinnersComponent },
+  { path: '**', component: GarageComponent },
+];
