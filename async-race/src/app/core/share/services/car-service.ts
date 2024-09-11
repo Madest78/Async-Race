ae5daee31f38043e1e05358d6b39cbae7e7a57a4
YuryMatusevich <vfnectdbx78@gmail.com>
2024-09-11 18:51:49 +0400

    feat: add deleteCar method

---
+++ async-race/src/app/core/share/services/car-service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Car, NewCar } from '../../components/pages/app.garage/models/car.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CarService {
+  private carsSubject = new BehaviorSubject<Car[]>([]);
+
+  cars$ = this.carsSubject.asObservable();
+
+  private endpoint = 'http://localhost:3000/garage';
+
+  constructor(private http: HttpClient) {
+    this.loadCars();
+  }
+
+  loadCars(): void {
+    this.http.get<Car[]>(this.endpoint).subscribe((cars) => {
+      this.carsSubject.next(cars);
+    });
+  }
+
+  createCar(car: NewCar): Observable<Car> {
+    return this.http.post<Car>(this.endpoint, car).pipe(
+      tap((newCar: Car) => {
+        const currentCars = this.carsSubject.value;
+        this.carsSubject.next([...currentCars, newCar]);
+      }),
+    );
+  }
+
+  deleteCar(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.endpoint}/${id}`).pipe(
+      tap(() => {
+        const currentCars = this.carsSubject.value.filter((car) => car.id !== id);
+        this.carsSubject.next(currentCars);
+      }),
+    );
+  }
+}
